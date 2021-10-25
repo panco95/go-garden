5edb0c467a4010acaa6520353c785de080e765e0
Panco <1129443982@qq.com>
2021-10-25 14:26:05 +0800

    修复rpc调用nil span引发指针错误

--- core/rpc.go
+++ core/rpc.go
@@ -30,18 +30,19 @@ func (g *Garden) RpcCall(span opentracing.Span, addr, service, method string, ar
 	xClient := client.NewXClient(service, client.Failtry, client.RandomSelect, d, client.DefaultOption)
 	defer xClient.Close()
 
-	textMap := opentracing.TextMapCarrier{}
-	opentracing.GlobalTracer().Inject(
-		span.Context(),
-		opentracing.TextMap,
-		textMap)
-
-	// write opentracing span to textMap
 	textMapString := map[string]string{}
-	textMap.ForeachKey(func(key, val string) error {
-		textMapString[key] = val
-		return nil
-	})
+	if span != nil {
+		textMap := opentracing.TextMapCarrier{}
+		opentracing.GlobalTracer().Inject(
+			span.Context(),
+			opentracing.TextMap,
+			textMap)
+		// write opentracing span to textMap
+		textMap.ForeachKey(func(key, val string) error {
+			textMapString[key] = val
+			return nil
+		})
+	}
 
 	ctx := context.WithValue(context.Background(), share.ReqMetaDataKey, textMapString)
 	err = xClient.Call(ctx, method, args, reply)
