333702fd569569fc113cc477d7c6c7b6526e5d20
Panco <1129443982@qq.com>
2021-12-18 10:57:09 +0800

    fix example

--- examples/pay/main.go
+++ examples/pay/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/panco95/go-garden/core"
+	"github.com/panco95/go-garden/examples/pay/api"
+	"github.com/panco95/go-garden/examples/pay/global"
+	"github.com/panco95/go-garden/examples/pay/rpc"
 )
 
 func main() {
-	data := map[string]interface{}{
-		"menu1": 1000,
-		"menu2": 2000,
-		"menu3": 3000,
-	}
-	body, err := core.PushGateway("test1111", data)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(body)
-	//global.Garden = core.New()
-	//global.Garden.Run(api.Routes, new(rpc.Rpc), global.Garden.CheckCallSafeMiddleware)
+	global.Garden = core.New()
+	global.Garden.Run(api.Routes, new(rpc.Rpc), global.Garden.CheckCallSafeMiddleware)
 }
